cdbecd6bf398abb60e401f4cd5765d761c514bf3
Eduardo Júnior <efduardo@outlook.com>
2022-04-06 18:10:55 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-#O João sempre leva para sua escola as maçãs que sobram do depósito do pai, distribuindo-as entre os colegas que encontra no início das aulas. Como o João ainda não aprendeu divisão, João precisa de um programa para receber como resposta quantas maçãs ele deve distribuir para cada colega. Ele não pode usar uma faca, deverá fazer uma distribuição de "maças inteiras" e o que sobrar deverá deixar na secretaria da escola.
+#O João sempre leva para sua escola as maçãs que sobram do depósito do pai, distribuindo-as entre os colegas que encontra no início das aulas. Como o João ainda não aprendeu divisão, ele precisa de um programa para receber como resposta quantas maçãs deve distribuir para cada colega. Ele não pode usar uma faca, deverá fazer uma distribuição de "maças inteiras" e o que sobrar deverá deixar na secretaria da escola.
 
 #Este programa dirá a quantia de maçãs a ser entregue a cada aluno.
 
