1b2b9fc9f5ac3b3e445c0d18ef63576208ac6e6d
Eduardo Júnior <efduardo@outlook.com>
2022-04-06 17:19:40 -0300

    Update and rename python1.py to um.py

--- um.py
+++ um.py
@@ -1,4 +1,7 @@
-#João tem n maçãs e deve distribuí-las igualmente entre seus colegas de turma e as que restarem deve entregar na secretaria da escola
+#Este é um programa para ser rodado em Python
+
+print("João tem n maçãs e deve distribuí-las igualmente entre seus colegas de turma e as que restarem deve entregar na secretaria da escola")
+print("")
 
 entrada1 = int(input("1) Digite o número total de maçãs: "))
 entrada2 = int(input("2) Digite o número de alunos: "))
@@ -6,6 +9,8 @@ inteirodadivisao = (entrada1 // entrada2)
 restodadivisao = (entrada1 % entrada2)
 
 while entrada1 >= 0 and entrada2 > 0:
+  print("")
   print("O número de maçãs a ser entregue a cada aluno é", inteirodadivisao)
-  print("Logo, o número de maçãs a ser entregue na secretaria é", restodadivisao)
+  print("")
+  print("Sendo assim, o número de maçãs restantesna secretaria é", restodadivisao)
   break
