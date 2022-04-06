4b17ce6821c0d8ccaae7f6ef7d81b8e3cacc0236
Eduardo Júnior <efduardo@outlook.com>
2022-04-06 17:42:35 -0300

    Update and rename um.py to 1.py

---
+++ 1.py
@@ -0,0 +1,6 @@
+#Este programa vai ler um número inteiro e imprimir seu quadrado.
+
+entrada = int(input("Digite um número inteiro: "))
+quadrado = entrada**2
+
+print("O quadrado do número digitado é", quadrado)
