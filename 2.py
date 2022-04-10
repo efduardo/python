b17d29f0f0950a770271eebe84884ce688f17b46
Eduardo Júnior <efduardo@outlook.com>
2022-04-10 08:34:48 -0300

    Delete 2.py

--- 2.py
+++
@@ -1,7 +0,0 @@
-#Este programa vai ler dois números inteiros e imprimir a sua soma.
-
-entrada1 = int(input("1) Digite um número inteiro: "))
-entrada2 = int(input("2) Digite outro número inteiro: "))
-soma = entrada1 + entrada2
-
-print("A soma dos números digitados é", soma)
