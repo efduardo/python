886c0e61dbee42701e6fc77ec4d5f97d20ed7e5a
Eduardo Júnior <efduardo@outlook.com>
2022-04-10 08:34:49 -0300

    Delete 3.py

--- 3.py
+++
@@ -1,7 +0,0 @@
-#Este programa vai ler dois números inteiros e imprimir o quadrado da diferença entre eles.
-
-entrada1 = int(input("Digite um número inteiro: "))
-entrada2 = int(input("Digite outro número inteiro"))
-quadradodadiferenca = (entrada1 - entrada2)**2
-
-print("O quadrado da diferenca dos números digitados é", quadradodadiferenca)
