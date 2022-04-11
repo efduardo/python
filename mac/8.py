be94d8b1b2c3795dcfa7979640298a190711cb20
Eduardo Júnior <efduardo@outlook.com>
2022-04-11 17:09:20 -0300

    Create 8.py

---
+++ mac/8.py
@@ -0,0 +1,5 @@
+#Este algoritmo solicita que o usuário digite 2 valores inteiros, armazenando o primeiro em uma variável e o segundo em outra variável; feito isto, utiliza uma nova variável (auxiliar) para trocar o conteúdo armazenado na primeira variável com o da segunda variável; finalmente imprime o valor da segunda e o da primeira, nesta ordem!
+
+variavel1 = int(input("Digite um número inteiro: "))
+variavel2 = int(input("Digite outro número inteiro: "))
+auxiliar = print(variavel2, variavel1)
