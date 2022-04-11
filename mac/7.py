21d6870a2b68b9c11377fce92ec34db2d2ceded0
Eduardo Júnior <efduardo@outlook.com>
2022-04-11 17:03:25 -0300

    Create 7.py

---
+++ mac/7.py
@@ -0,0 +1,9 @@
+#Este é um algoritmo que recebe dois números inteiros como entradas e imprime 1 se ocorrer entrada1 < entrada2, e 0 se ocorrer entrada1 >= entrada2.
+
+entrada1 = int(input("Digite um número inteiro: "))
+entrada2 = int(input("Digite putro número inteiro: "))
+
+if entrada1 < entrada2:
+    print(1)
+elif entrada1 >= entrada2:
+    print(0)
