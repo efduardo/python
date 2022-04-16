5ff87206db2f50dfabdcb91f6fd52ab75d15564e
Eduardo Júnior <efduardo@outlook.com>
2022-04-15 22:25:16 -0300

    Create bhaskara.py

---
+++ utilities/bhaskara.py
@@ -0,0 +1,20 @@
+Esta é uma calculadora para funções quadráticas.
+
+import math
+
+print("Tendo uma equação do tipo ax² + bx + c = 0, vamos encontrar os valores de x")
+
+a = float(input("\nDigite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+delta = (b**2) - (4*a*c)
+while delta >= 0:
+    raizdedelta = math.sqrt(delta)
+    x1 = (- b + raizdedelta)/(2*a)
+    x2 = (- b - raizdedelta)/(2*a)
+    if raizdedelta > 0:
+        print("\nOs possíveis valores de x são", x1, "e", x2)
+    elif raizdedelta == 0:
+        print("\nA função possui raiz biquadrada igual a", x1)
+    break
