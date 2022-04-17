a0fa1a4ea49e328694fd4ef04e44fa126edee962
Eduardo Júnior <efduardo@outlook.com>
2022-04-17 10:20:17 -0300

    Create elementos químicos.py

---
+++ utilities/elementos químicos.py
@@ -0,0 +1,32 @@
+#Este é um programa que permite que o usuário encontre as propriedades dos elementos químicos presentes na Tabela Periódica.
+
+z = float(input("\nDigite o número atômico do elemento: "))
+
+if z == 1:
+    print("\nNome: Hidrogênio \nSímbolo: [H] \nTipo: Ametal \nNº atômico: 1; \nNº de prótons/elétrons: 1; \nMassa molecular: 1.008u; \nConfiguração eletrônica: 1s^1\n")
+elif z == 2:
+    print("\nNome: Helio \nSímbolo: [He] \nTipo: Gás nobre \nNº atômico: 2; \nNº de prótons/elétrons: 2; \nMassa molecular: 4.003u; \nConfiguração eletrônica: 1s^2\n")
+elif z == 3:
+    print("\nNome: Litio \nSímbolo: [Li] \nTipo: Metal alcalino \nNº atômico: 3; \nNº de prótons/elétrons: 3; \nMassa molecular: 6.941u; \nConfiguração eletrônica: 1s^2, 2s^1\n")
+elif z == 4:
+    print("\nNome: Helio \nSímbolo: [Be] \nTipo: Metal alcalino-terroso \nNº atômico: 4; \nNº de prótons/elétrons: 4; \nMassa molecuclar: 9.012u; \nConfiguração eletrônica: 1s^2, 2s^2\n")
+elif z == 5:
+    print("\nNome: Boro \nSímbolo: [B] \nTipo: Semimetal \nNº atômico: 5; \nNº de prótons/elétrons: 5; \nMassa molecular: 10.811u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^1\n")
+elif z == 6:
+    print("\nNome: Carbono \nSímbolo: [C] \nTipo: Ametal \nNº atômico: 6; \nNº de prótons/elétrons: 6; \nMassa molecular: 12.011u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^2\n")
+elif z == 7:
+    print("\nNome: Nitrogênio \nSímbolo: [N] \nTipo: Ametal \nNº atômico: 7; \nNº de prótons/elétrons: 7; \nMassa molecuclar: 14.007u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^3\n")
+if z == 8:
+    print("\nNome: Oxigênio \nSímbolo: [O] \nTipo: Ametal \nNº atômico: 8; \nNº de prótons/elétrons: 8; \nMassa molecular: 15.999u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^4\n")
+elif z == 9:
+    print("\nNome: Fluor \nSímbolo: [F] \nTipo: Halogênio \nNº atômico: 9; \nNº de prótons/elétrons: 9; \nMassa molecular: 18.998u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^5\n")
+elif z == 10:
+    print("\nNome: Neônio \nSímbolo: [Ne] \nTipo: Gás nobre \nNº atômico: 10; \nNº de prótons/elétrons: 10; \nMassa molecular: 20.18u; \nConfiguração eletrônica: 1s^2, 2s^2, 2p^6\n")
+elif z == 11:
+    print("\nNome: Sódio \nSímbolo: [Na] \nTipo: Metal alcalino \nNº atômico: 11; \nNº de prótons/elétrons: 11; \nMassa molecuclar: 20.99u; \nConfiguração eletrônica: 3s^1\n")
+elif z == 12:
+    print("\nNome: Magnésio \nSímbolo: [Mg] \nTipo: Metal alcalino-terroso \nNº atômico: 12; \nNº de prótons/elétrons: 12; \nMassa molecular: 24.305u; \nConfiguração eletrônica: 3s^2\n")
+elif z == 13:
+    print("\nNome: Alumínio \nSímbolo: [Al] \nTipo: Metal \nNº atômico: 13; \nNº de prótons/elétrons: 13; \nMassa molecular: 26.982u; \nConfiguração eletrônica: 3s^2, 3p^1\n")
+elif z == 14:
+    print("\nNome: Silício \nSímbolo: [Si] \nTipo: Semimetal \nNº atômico: 14; \nNº de prótons/elétrons: 14; \nMassa molecuclar: 28.086u; \nConfiguração eletrônica: 3s^2, 3p^2\n")
