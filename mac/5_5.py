dfcf9028d522652b273d89a63c9a5d694105ab5a
Eduardo Júnior <efduardo@hotmail.com>
2022-04-19 21:22:26 -0300

    Update 5_5.py

--- mac/5_5.py
+++ mac/5_5.py
@@ -1,6 +1,6 @@
 #Este é um programa que lê um número natural e computa a maior soma de naturais consecutivos, que seja menor ou igual ao valor digitado.
 
-#Se o usuário digitar um número n, então o programa irá computar a somatória 0 + 1 +...+ x = n.
+#Se o usuário digitar um número n, então o programa irá computar a somatória 0 + 1 +... = x, onde x <= n.
 
 n = int(input("Digite um número inteiro: "))
 
