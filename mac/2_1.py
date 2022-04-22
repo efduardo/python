9c4a0917b840d6ccdfe688ec82e6f27d50811b61
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 21:25:36 -0300

    Update 2_1.py

--- mac/2_1.py
+++ mac/2_1.py
@@ -1,6 +1,7 @@
 #Este algoritmo vai ler um número inteiro e imprimir o seu quadrado.
 
 entrada = int(input("Digite um número inteiro: "))
+
 quadrado = entrada**2
 
 print("O quadrado do número digitado é", quadrado)
