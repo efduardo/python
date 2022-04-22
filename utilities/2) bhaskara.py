f327eec92f46cf232b2685178ec71de9977a3fac
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 21:34:19 -0300

    Update 2) bhaskara.py

--- utilities/2) bhaskara.py
+++ utilities/2) bhaskara.py
@@ -9,10 +9,12 @@ b = float(input("2)Digite o valor de b: "))
 c = float(input("3)Digite o valor de c: "))
 
 delta = (b**2) - (4*a*c)
+
 while delta >= 0:
     raizdedelta = math.sqrt(delta)
     x1 = (- b + raizdedelta)/(2*a)
     x2 = (- b - raizdedelta)/(2*a)
+    
     if raizdedelta > 0:
         print("\nOs possíveis valores de x são", x1, "e", x2)
     else:
