01a10325dfd461ec5c8860115cc910e914b72f45
Eduardo Júnior <efduardo@hotmail.com>
2022-04-21 21:27:07 -0300

    Update 2_3.py

--- mac/2_3.py
+++ mac/2_3.py
@@ -2,6 +2,7 @@
 
 entrada1 = int(input("Digite um número inteiro: "))
 entrada2 = int(input("Digite outro número inteiro"))
+
 quadradodadiferenca = (entrada1 - entrada2)**2
 
 print("O quadrado da diferenca dos números digitados é", quadradodadiferenca)
