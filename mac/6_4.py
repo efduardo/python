8ad4c3450fe6c60bf4268e5ac648270528c65ef9
Eduardo Júnior <efduardo@hotmail.com>
2022-04-27 12:08:48 -0300

    Create 6_4.py

---
+++ mac/6_4.py
@@ -0,0 +1,14 @@
+#Este programa solicita que o usuário digite um inteiro n e, como saída, imprime a soma dos dígitos do número n, e em seguida a unidade do número n.
+
+n = int(input("Digite um número inteiro: "))
+
+soma = 0
+
+while (n != 0):
+    resto = n % 10
+    n = (n - resto)//10
+    soma += resto
+    
+unidade = soma%10
+
+print(soma, unidade)
