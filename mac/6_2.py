4f120cf1dfc041bc9a7dbd694a4aea86c721ac80
Eduardo Júnior <efduardo@hotmail.com>
2022-04-26 22:28:50 -0300

    Create 6_2.py

---
+++ mac/6_2.py
@@ -0,0 +1,17 @@
+#Este programa solicita que o usuário digite um inteiro positivo n, então implementa um laço (while) que computa a soma dos n primeiros números pares até o número digitado, imprimindo a soma final. O inteiro 0 é tomado como o primeiro número par.
+
+n = int(input())
+
+i = 1
+p = 0
+
+while (n >= 0) and (i != p) and p <= n:
+    i += 2
+    p += 2
+    a = p - 2
+
+t = int((n / 2) + 1)
+an = 0 + (t - 1)*2
+sn = (0 + an) * (t / 2)
+
+print(int(sn))
