d95065466960aac048a6c2e1ac0141857921de13
Eduardo Júnior <efduardo@hotmail.com>
2022-04-27 00:11:27 -0300

    Create 6_3.py

---
+++ mac/6_3.py
@@ -0,0 +1,23 @@
+#Este programa solicita que o usuário digite um inteiro positivo n, então implementa um laço (while) que computa a soma dos n primeiros números ímpares até o número digitado, imprimindo a soma final.
+
+n = int(input("Digite um número inteiro positivo: "))
+
+i = 1
+p = 0
+
+while (n > 0) and (i != p) and (i <= n):
+    i += 2
+    p += 2
+    a = i - 2
+
+if p == n:
+    t = n/2
+    an = 1 + (t-1)*2
+    sn = (1 + an)*t/2
+    print(int(sn))
+else:
+    a += 1
+    t = (n + 1)/2
+    an = 1 + (t - 1)*2
+    sn = (1 + an) * (t / 2)
+    print(int(sn))
